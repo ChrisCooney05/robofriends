da14641bdef2e39abb4ae061683b582f6bf2ca9a
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-21 08:02:24 +0100

    using a loop to create a card for each robot instead of doing it manually

--- src/CardList.js
+++ src/CardList.js
@@ -4,13 +4,10 @@ import Card from "./Card";
 class CardList extends React.Component {
   render() {
     const { robots } = this.props;
-    return (
-      <div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-      </div>
-    );
+    const robotCardsArray = robots.map((robot) => {
+      return <Card id={robot.id} name={robot.name} email={robot.email} />;
+    });
+    return <div>{robotCardsArray}</div>;
   }
 }
 
