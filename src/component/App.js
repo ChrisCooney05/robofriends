5b788efeff01be4ec60f4bb03e0520b1517554f0
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-22 08:17:56 +0100

    onSearchChange() added and passed into SearchBox only logs event currently

--- src/component/App.js
+++ src/component/App.js
@@ -4,15 +4,27 @@ import SearchBox from "./SearchBox";
 import { robots } from "../robots";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchField: "",
+    };
+  }
+
+  onSearchChange(event) {
+    console.log(event);
+  }
+
   render() {
     return (
       <div className="tc">
         <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={robots} />;
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList robots={this.state.robots} />;
       </div>
     );
   }
 }
-
+//we construct a onSearchChange method and pass that in as a prop to SearchBox
 export default App;
--- src/component/SearchBox.js
+++ src/component/SearchBox.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 
 class SearchBox extends Component {
   render() {
+    const { onSearchChange } = this.props;
     return (
       <div className="pa2">
         <input
           className="pa3 ba b--green bg-lightest-blue"
           type="search"
           placeholder="Search Robots"
+          onChange={onSearchChange} //onChange picks up when txt is entered and executes function
         />
       </div>
     );
