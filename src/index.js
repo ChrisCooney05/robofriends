9954aafa8bf9ac593b724c3d6ecabd29620841e4
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-22 07:28:27 +0100

    added App component to control all others

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import CardList from "./CardList";
+import App from "./component/App";
 import "tachyons";
 import * as serviceWorker from "./serviceWorker";
-import { robots } from "./robots";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      <CardList robots={robots} />
-    </div>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
