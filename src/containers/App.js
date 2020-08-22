9e1497207b9e843cf11f7be75cf77024de647042
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-22 17:39:26 +0100

    Redux hooked up need to cover again

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,44 +6,37 @@ import ErrorBoundary from "../components/ErrorBoundary";
 import { connect } from "react-redux";
 import "../app.css";
 
-import { setSearchField } from "../actions";
+import { setSearchField, requestRobots } from "../actions";
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField,
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots()),
   };
 };
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-    };
-  }
-
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json()) //turn response into json
-      .then(users => this.setState({ robots: users }));
+    this.props.onRequestRobots();
   }
-  //we now fetch users from an api and store that in state after App has mounted
 
   //we filter the robots we have in state against the searchfield with have stored in state to return any matches
   render() {
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
+    const { searchField, robots, onSearchChange, isPending } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
     //0 evaluates as false and ! turns it into true so its easier to read vs .length === 0
-    if (!robots.length) {
+    if (isPending) {
       return <h1 className="tc">Loading...</h1>;
     } else {
       return (
--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./containers/App";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import "tachyons";
 import * as serviceWorker from "./serviceWorker";
-import { searchRobots } from "./reducers";
+import { searchRobots, requestRobots } from "./reducers";
 
 const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
 const store = createStore(
-  searchRobots,
+  rootReducer,
   applyMiddleware(thunkMiddleware, logger)
 );
 
