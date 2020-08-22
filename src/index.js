b35326ed7c8370faac3171b1e588754dc192872f
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-22 17:24:35 +0100

    New reducer added to handle api call

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import App from "./containers/App";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
 import "tachyons";
 import * as serviceWorker from "./serviceWorker";
 import { searchRobots } from "./reducers";
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(
+  searchRobots,
+  applyMiddleware(thunkMiddleware, logger)
+);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,10 @@
 /* eslint-disable default-case */
-import { CHANGE_SEARCH_FIELD } from "./constants";
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED,
+} from "./constants";
 
 const initialState = {
   searchField: "",
@@ -14,3 +19,28 @@ export const searchRobots = (state = initialState, action = {}) => {
   }
 };
 //an alternatives to Object.assign is {...state, searchField: action.payload } spread operator is les verbose
+
+const robotsInitialState = {
+  isPending: false,
+  robots: [],
+  error: "",
+};
+
+export const requestRobots = (state = robotsInitialState, action = {}) => {
+  switch (action.type) {
+    case REQUEST_ROBOTS_PENDING:
+      return Object.assign({}, state, { isPending: true });
+    case REQUEST_ROBOTS_SUCCESS:
+      return Object.assign({}, state, {
+        robots: action.payload,
+        isPending: false,
+      });
+    case REQUEST_ROBOTS_FAILED:
+      return Object.assign({}, state, {
+        error: action.payload,
+        isPending: false,
+      });
+    default:
+      return state;
+  }
+};
