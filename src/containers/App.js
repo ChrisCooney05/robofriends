bb15a0efdf9ba9bb4b365084fccec287244cb878
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-22 17:00:33 +0100

    redux now controlls the state of searchField

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,33 +3,43 @@ import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
+import { connect } from "react-redux";
 import "../app.css";
+
+import { setSearchField } from "../actions";
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+  };
+};
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       robots: [],
-      searchField: "",
     };
   }
-  //by using an arrow function we can keep the value of this. as the App class, if not we will get an undefined
-  //error back
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value }); //uses to change the state object, must use setState()
-  };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json()) //turn response into json
-      .then((users) => this.setState({ robots: users }));
+      .then(res => res.json()) //turn response into json
+      .then(users => this.setState({ robots: users }));
   }
   //we now fetch users from an api and store that in state after App has mounted
 
   //we filter the robots we have in state against the searchfield with have stored in state to return any matches
   render() {
-    const { robots, searchField } = this.state;
-    const filteredRobots = robots.filter((robot) => {
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
+    const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
     //0 evaluates as false and ! turns it into true so its easier to read vs .length === 0
@@ -39,7 +49,7 @@ class App extends Component {
       return (
         <div className="tc">
           <h1 className="f1">RoboFriends</h1>
-          <SearchBox onSearchChange={this.onSearchChange} />
+          <SearchBox onSearchChange={onSearchChange} />
           <Scroll>
             <ErrorBoundary>
               <CardList robots={filteredRobots} />;
@@ -53,4 +63,4 @@ class App extends Component {
 //scroll is a wrapper class
 //ErrorBoundary will catch any errors we get
 //we construct a onSearchChange method and pass that in as a prop to SearchBox
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
